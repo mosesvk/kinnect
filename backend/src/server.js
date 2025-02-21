990393ce0b436839edbda4896d28fcec5b10c1e7
mosesvk <kaumatule93@gmail.com>
2025-02-21 16:22:41 -0700

    user feature backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const connectDB = require('./config/db.js')
 const dotenv = require('dotenv')
+const userRoutes = require('./routes/userRoutes.js')
+const cors = require('cors')
+const helmet = require('helmet')
 
 dotenv.config()
 
@@ -8,10 +11,16 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet())
+
 
 app.get('/', (req, res) => {
     res.send('API is sending...')
 })
+
+app.get('/api/users', userRoutes)
 
 connectDB()
 
